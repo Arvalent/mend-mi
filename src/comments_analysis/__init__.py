35b17a24610be325109e0da014c22645e91a844a
LucasBrunschwig <lucas.brunschwig@gmail.com>
2022-12-03 14:45:51 +0100

    set up fixed

---
+++ src/comments_analysis/__init__.py
@@ -0,0 +1,1 @@
+from comments_extraction import *
--- src/comments_analysis/comments_extraction.py
+++ src/comments_analysis/comments_extraction.py
@@ -1,5 +1,9 @@
 """
-Description: this file receives
+Description: this file defines method that can be used to extract comments from different platform for a specific topic
+
+Platform:
+    - Google: location and comments about these location
+    - TripAdvisor: restaurants name or url link and find object
 
 
 """
